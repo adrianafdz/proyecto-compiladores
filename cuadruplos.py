77f5c0674fc3408b75236555898425816e7b346a
Jeasika López <33463615+jeasika@users.noreply.github.com>
2022-05-05 20:32:15 -0500

    Update cuadruplos

--- cuadruplos.py
+++ cuadruplos.py
@@ -1,3 +1,5 @@
+from tablaVars import tablaVars
+
 class Cuadruplos():
     def __init__(self):
         self.cuadruplos = list()
@@ -10,3 +12,31 @@ class Cuadruplos():
         for c in self.cuadruplos:
             output += str(c) + '\n'
         return output
+    
+    #Leer: input()
+    def read(self, cuadruplos, nombre):
+        if (cuadruplos[0] == 'leer'):
+            texto = tablaVars.search_var(nombre)
+            if (tablaVars.searchType_var == 'number'):
+                print ("Valor tipo number") #Aplicar funcion to_string 
+            elif(tablaVars.searchType_var == 'string'):
+                print ("Valor tipo string")
+        else:
+            print("error")
+
+    #Escribir: print(<string>)
+    def write(self, cuadruplos, cadena):
+        if (cuadruplos[0] == 'escribir'):
+            texto = tablaVars.search_var(cadena)
+            print(">>", str(texto))
+        else:
+            print("error")
+
+    #Regresar 
+    def ret(self, cuadruplos, valor):
+        stackRetorno = []
+        if (cuadruplos[0] == 'regresar'):
+            texto = tablaVars.search_var(valor)
+            stackRetorno.append(texto) #Guarda el valor 
+        else:
+            print("error")
