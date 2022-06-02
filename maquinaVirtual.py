38e319c45599349edc602ecf3b376027d62c85f0
Jeasika López <jesika_18_@hotmail.com>
2022-06-02 15:56:05 -0500

    Cuadruplos a ejecutar, cambio en direcciones

--- maquinaVirtual.py
+++ maquinaVirtual.py
@@ -1,5 +1,5 @@
 import sys
-from aloop import *
+#from aloop import *
 from dirVirtuales import *
 
 # Declaracion de constantes
@@ -79,12 +79,16 @@ Funcion para obtener el tipo de la direccion
 def getTipo(direccion):
     print ("Tipo = ", direccion)
     direccion = int(direccion)
-    if ((direccion >= 0 and direccion <= 2999) or (direccion >= 5000 and direccion <= 7999) or (direccion >= 10000 and direccion <= 12999) or (direccion >= 17000 and direccion <= 21999) ):
+    if ((direccion >= BASE_DIRGLOBALNUM_LI and direccion <= BASE_DIRGLOBALNUM_LS) or (direccion >= BASE_DIRLOCALNUM_LI and direccion <= BASE_DIRLOCALNUM_LS) or (direccion >= BASE_DIRTEMPNUM_LI and direccion <= BASE_DIRTEMPNUM_LS) or (direccion >= BASE_DIRCONSTNUM_LI and direccion <= BASE_DIRCONSTNUM_LS) ):
         return 'number'
-    if ((direccion >= 3000 and direccion <= 4999) or (direccion >= 8000 and direccion <= 9999) or (direccion >= 13000 and direccion <= 13999) or (direccion >= 22000 and direccion <= 26999) ):
+    if ((direccion >= BASE_DIRGLOBALSTR_LI and direccion <= BASE_DIRGLOBALSTR_LS) or (direccion >= BASE_DIRLOCALSTR_LI and direccion <= BASE_DIRLOCALSTR_LS) or (direccion >= BASE_DIRTEMPSTR_LI and direccion <= BASE_DIRTEMPSTR_LS) or (direccion >= BASE_DIRCONSTSTR_LI and direccion <= BASE_DIRCONSTSTR_LS) ):
         return 'string'
-    if (direccion >= 14000 and direccion <= 14999):
+    if (direccion >= BASE_DIRTEMPBOOL_LI and direccion <= BASE_DIRTEMPBOOL_LS):
         return 'bool'
+    if (direccion >= BASE_DIRTEMPPOINTNUM_LI and direccion <= BASE_DIRTEMPPOINTNUM_LS):
+        return 'pointer number'
+    if (direccion >= BASE_DIRTEMPPOINTSTR_LI and direccion <= BASE_DIRTEMPPOINTSTR_LS):
+        return 'pointer string'
     else:
         print("Error Maquina Virtual: {} no se encuentra dentro del rango de ningun tipo de variable".format(direccion))
         sys.exit()
@@ -94,6 +98,7 @@ Funcion para meter los valores en la memoria
 '''
 def llenarValor(memoriaVirtual, memDireccion, memTipo, valor):
     global memGlobal
+    print (memDireccion)
     #obtiene el valor de la direccion
     #guarda el valor en la memoria principal
 
@@ -102,9 +107,9 @@ Funcion para realizar operaciones aritmeticas
 '''
 def resOpeBool(res):
     if res == True:
-        print ('1')
+        return 1
     elif res == False:
-        print ('0')
+        return 0
 
 def operadores(signo):
     global cuadruplos
@@ -128,69 +133,99 @@ def operadores(signo):
         elif signo == '/':
             res = val1 / val2
         elif signo == '>':
-            res = val1 > val2
-            resOpeBool(res)
+            res = resOpeBool(val1 > val2)
         elif signo == '<':
-            res = val1 < val2
-            resOpeBool(res)
+            res = resOpeBool(val1 < val2)
         elif signo == '<>':
-            res = val1 != val2
-            resOpeBool(res)
+            res = resOpeBool(val1 != val2)
         elif signo == '==':
-            res = val1 == val2
-            resOpeBool(res)
+            res = resOpeBool(val1 == val2)
 
     elif tipo1 == 'string' and tipo2 == 'string':
         if  signo == '<>':
-            res = val1 != val2
-            resOpeBool(res)
+            res = resOpeBool(val1 != val2)
         elif signo == '==':
-            res = val1 == val2
-            resOpeBool(res)
-        '''
-        Operacion con '&' 
+            res = resOpeBool(val1 == val2)
         elif signo == '&':
-            res
-        ''' 
+            res = val1 + val2
     
     llenarValor(pilaCorriendo, cuadruplos[3], getTipo(cuadruplos[3]), res)
 
-    def main():
-        Finish = False
-        while not Finish:
-            sigCuadIndice = -1
-            pilaCorriendo = top(CONST_EJECUCION)
-            cuadruplos = cuadLista[cuadIndice] #Cuadruplo a ejecutar
+'''
+DECLARACION DE VARIABLES GLOBALES
+'''
+cuadIndice = 0
 
-            # Asignacion
-            if cuadruplo[0] == '=':
-                try:  
-                    valor = getValor(pilaCorriendo, cuadruplo[1], getTipo(cuadruplo[1]))
-                except:
-                    valor = pop(CONST_RETORNO_VALOR)
-                
-                llenarValor(pilaCorriendo, cuadruplo[3], getTipo(cuadruplo[3]), valor)
+def main():
+    Termina = False
+    #print (cuadruplo[0])
+    while not Termina:
+        sigCuadIndice = -1
+        pilaCorriendo = top(CONST_EJECUCION)
+        print ("Ejecutando cuadruplo = ", cuadruplos[cuadIndice])
+        cuadruplo = cuadruplos[cuadIndice]
+        print ("Operacion = ", cuadruplo[0])
 
-            # Lectura
-            elif cuadruplo[0] == 'READ':
-                auxTipo = getTipo(cuadruplo[1])
-                #saber que tipo de valor esta leyendo para guardarlo en el espacio de memoria correspondiente
-
-            # Escritura
-            elif cuadruplo[0] == 'PRINT':
-                texto = getValor(pilaCorriendo, cuadruplo[1], getTipo(cuadruplo[1]))
-                print("<< ",str(texto))
+        # Asignacion
+        if cuadruplo[0] == '=':
+            try:  
+                valor = getValor(pilaCorriendo, cuadruplo[1], getTipo(cuadruplo[1]))
+            except:
+                valor = pop(CONST_RETORNO_VALOR)
             
-            # GOTO
-            # GOTOF
-            # GOTOV
-            # GOSUB
-            # ERA
-            # PARAMETER
-            # ENDFUNC
-            # OBJREF
-            # SUMREF
-            # VERIFY
-            # CNUM
-            # CSTR
-            # RET
+            llenarValor(pilaCorriendo, cuadruplo[3], getTipo(cuadruplo[3]), valor)
+
+        # Lectura
+        elif cuadruplo[0] == 'READ':
+            auxTipo = getTipo(cuadruplo[1])
+            #saber que tipo de valor esta leyendo para guardarlo en el espacio de memoria correspondiente
+
+        # Escritura
+        elif cuadruplo[0] == 'PRINT':
+            texto = getValor(pilaCorriendo, cuadruplo[1], getTipo(cuadruplo[1]))
+            print("<< ",str(texto))
+        
+        # GOTO
+        elif cuadruplo[0] == 'GOTO':
+            sigCuadIndice = int(cuadruplo[3])
+            print (sigCuadIndice)
+        # GOTOF
+        # GOTOV
+        # GOSUB
+        # ERA
+        # PARAMETER
+        # ENDFUNC
+        #elif cuadruplo[0] == 'ENDFUNC':
+        
+        # OBJREF
+        # SUMREF
+        # VERIFY
+        # CNUM
+        # CSTR
+        # RET
+
+        #Control de índices para el cuadIndice o sigCuadIndice
+        # if sigCuadIndice != -1:
+        #     cuadIndice = sigCuadIndice
+        # else: 
+        #     cuadIndice = cuadIndice + 1
+        
+        Termina = True
+
+'''
+EJECUTAR EL ARCHIVO CON EL CODIGO INTERMEDIO Y
+PASA LOS CUADRUPLOS COMO TUPLA A UNA LISTA PARA PODER SER ACCESADOS
+'''
+cuadLista = []
+cuadruplos = []
+
+codObj = open("codint.txt", 'r', encoding='utf-8')
+cuadLista = codObj.readlines()
+
+for linea in cuadLista:
+    linea = linea.replace('\n', '')
+    cuadruplo = tuple(linea.split(','))
+    cuadruplo = (cuadruplo[0], cuadruplo[1], cuadruplo[2], cuadruplo[3])
+    cuadruplos.append(cuadruplo)
+
+main()
